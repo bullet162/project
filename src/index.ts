dbb994a2c172791f54c8ae9f92aaae52c4ae30ab
Jherson Aguto <agutojherson@gmail.com>
2026-02-15 12:47:21 +0800

    Hash password and configs

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express from "express";
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.json());
+
+app.get("/test", (req, res) => {
+	res.status(200).send({ message: ["healthy", 200] });
+});
+
+app.listen(PORT, () => {
+	console.log(`Server listening: http://localhost:${PORT}`);
+});
